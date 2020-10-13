f122b707e48fb20479e9277a6155860922bfd523
bouhartsev <programmer@bouhartsev.site>
2020-10-13 12:03:49 +0300

    Add Lab4 and etc.

--- source/script/who.js
+++ source/script/who.js
@@ -3,6 +3,7 @@ function NotReady() {
     NotReadyDiv.style = "width: 100%; height: 100vh; font-size: 50px; padding: 30px; box-sizing: border-box;";
     NotReadyDiv.innerHTML = 'Здесь появится сайт <a href="https://vk.com/bouhartsev">Матвея Бухарцева</a>';
     document.body.prepend(NotReadyDiv);
+    
 }
 
 function Load() {
