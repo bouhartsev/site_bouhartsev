d022216c0f37e188e77c51ed5b58972c4e4a12ed
bouhartsev <programmer@bouhartsev.site>
2020-10-20 11:10:06 +0300

    Fix bag with header

--- source/script/main.js
+++ source/script/main.js
@@ -5,13 +5,13 @@ $(window).scroll(HeaderOnScroll);
 
 function HeaderOnScroll() {
     //Change header to fixed and reverse
-    if (($('#about').offset().top-window.pageYOffset <= heightHeaderFixed*window.innerHeight) && !($('#header').hasClass("headerFixed"))) {
+    if (($('#about').offset().top-window.pageYOffset <= 0) && !($('#header').hasClass("headerFixed"))) {
         $('#header').before('<div id="emptyHeader"></div>');
         $('#emptyHeader').height($('#header').height());
         $('#header').addClass("headerFixed");
         //window.pageYOffset = 0;
     }
-    else if ($('#about').offset().top-window.pageYOffset > heightHeaderFixed*window.innerHeight) {
+    else if ($('#about').offset().top-window.pageYOffset > 0) {
         $('#header').removeClass("headerFixed");
         $('#emptyHeader').remove();
     }
